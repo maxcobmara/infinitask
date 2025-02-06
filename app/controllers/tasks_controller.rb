1f7d6e84bf3dbca2d2caad7ad5e6a0e38515b81d
Yaacob Noor <maxcobmara@gmail.com>
2025-02-07 02:21:34 +0800

    update redirects after actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -37,7 +37,7 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: "Task was successfully created." }
+        format.html { redirect_to tasks_path, notice:  "Task <strong>#{@task.title.titleize}</strong> was created", flash: { html_safe: true } }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -50,7 +50,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: "Task was successfully updated." }
+        format.html { redirect_to tasks_path, notice: "Task was successfully updated." }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -69,7 +69,7 @@ class TasksController < ApplicationController
     @task.destroy!
 
     respond_to do |format|
-      format.html { redirect_to tasks_path, status: :see_other, notice: "Task was successfully destroyed." }
+      format.html { redirect_to tasks_path, status: :see_other, notice: "Task <strong>#{@task.title.titleize}</strong> was deleted", flash: { html_safe: true }}
       format.json { head :no_content }
     end
   end
