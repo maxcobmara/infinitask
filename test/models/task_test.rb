476c37486f4f2a4742a237b29b006d03c0fa643d
Yaacob Noor <maxcobmara@gmail.com>
2025-02-07 09:57:47 +0800

    minitest update on model

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -2,12 +2,20 @@ require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
   
-  def setup
-    @task = tasks(:one)
+  test "newly created tasks are uncomplete by default" do
+      task = tasks(:one)
+      assert_same(task.completed, false)
+  end
+  
+  test "tasks can be completed" do
+      task = tasks(:two)
+      assert_same(task.completed, true)
+  end
+  
+  test "should not save without title" do
+    task = tasks(:three)
+    assert_not task.save
   end
   
   
-  # test "the truth" do
-  #   assert true
-  # end
 end
